6e23694cb216bd7e7c24941a47f45b63dfef7184
leninner <mazabandalenin180@gmail.com>
2023-05-12 10:49:38 -0500

    add local data source implementation and tests

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,34 @@
+import 'dart:convert';
+
+import 'package:clean_architecture_tdd/core/error/exception.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+// ignore: constant_identifier_names
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString == null) throw CacheException();
+
+    return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    final triviaJson = json.encode(triviaToCache.toJson());
+
+    return sharedPreferences.setString(CACHED_NUMBER_TRIVIA, triviaJson);
+  }
 }
